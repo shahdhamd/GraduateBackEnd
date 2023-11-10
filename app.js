b994de4eab1600a704ab33949a26c3fab4d83123
shahdhamad <shhdh5296@gmail.com>
2023-11-10 15:37:46 +0200

    second commit

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ import  express  from "express";
 import dotenv from 'dotenv'
 import * as indexRouter from './src/modules/index.route.js'
 import connectDB from "./DB/connection.js";
+import cors from 'cors';
+import compression from 'compression'
 const app=express()
 dotenv.config()
 connectDB()
 app.use(express.json())
 const port=process.env.Port
 const baseUrl=process.env.BASEURL
+app.use(cors())
+app.use(compression())
 app.use(`${baseUrl}user`,indexRouter.userRouter)
 app.use(`${baseUrl}auth`,indexRouter.authRoter)
 app.use(`${baseUrl}herb`,indexRouter.herbRouter)
