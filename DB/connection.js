f7d6d9e4b7c9736ee42cdc0a19ea2e1b77909d20
shahdhamd <113027252+shahdhamd@users.noreply.github.com>
2023-11-25 21:19:04 +0200

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 
 const connectDB=async()=>{
 
-    return await mongoose.connect(process.env.DBURL)
+    return await mongoose.connect(process.env.DBURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000, // Set a timeout value in milliseconds
+      })
     .then (res=>{
         console.log("sucess connect")
     }).catch(error=>{
