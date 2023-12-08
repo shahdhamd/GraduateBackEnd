9b021f9c329761da4d8eb9accef689a7c8bc5d3f
shahdhamd <113027252+shahdhamd@users.noreply.github.com>
2023-12-08 11:41:55 +0200

    Update user.validation.js

--- src/modules/user/user.validation.js
+++ src/modules/user/user.validation.js
@@ -20,4 +20,10 @@ export const deleteUserAccount={
         id:joi.number().required()
     })
 }
-
+export const updateUserInfo={
+    body:joi.object().keys({
+        userName:joi.string().min(3).max(25),
+        email: joi.string(),
+        role:joi.string()
+    })
+}
