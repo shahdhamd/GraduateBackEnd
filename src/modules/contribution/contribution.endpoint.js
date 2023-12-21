bfbf3b5363fecba6a22ccdc1bae9584a87c2f1f7
shahdhamd <113027252+shahdhamd@users.noreply.github.com>
2023-12-21 10:15:18 +0200

    Delete src/modules/contribution directory

--- src/modules/contribution/contribution.endpoint.js
+++
@@ -1,8 +0,0 @@
-import { roles } from "../../services/roles.js";
-
-export const endpoint={
-    getAll:[roles.Admin],
-    update:[roles.Admin,roles.User],
-    createContribution:[roles.Admin,roles.User],
-    delete:[roles.User,roles.Admin]
-}
