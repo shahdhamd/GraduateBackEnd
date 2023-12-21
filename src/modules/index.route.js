4ae831672f47906e866c6354711902f6210a427b
shahdhamd <113027252+shahdhamd@users.noreply.github.com>
2023-12-21 10:14:23 +0200

    Update index.route.js

--- src/modules/index.route.js
+++ src/modules/index.route.js
@@ -1,7 +1,6 @@
 import userRouter from './user/user.router.js'
 import authRouter from './auth/auth.router.js'
 import herbRouter from './herb/herb.route.js'
-import contributionRouter from './contribution/contribution.route.js'
 export {
-    userRouter,authRouter,herbRouter,contributionRouter
+    userRouter,authRouter,herbRouter
 }
