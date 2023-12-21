871956d09a24a42e6e5c3f5b573fc9bca2168b95
shahdhamd <113027252+shahdhamd@users.noreply.github.com>
2023-12-21 10:34:20 +0200

    Update contribution.route.js

--- src/modules/contribution/contribution.route.js
+++ src/modules/contribution/contribution.route.js
@@ -8,7 +8,7 @@ import * as validateContribution from './contribution.validation.js'
 import { fileValidation, myMulter } from "../../services/multer.js";
 import { validation } from "../../middlewares/validation.js";
 router.delete('/:id',auth(endpoint.delete),contribution.deleteContribution)
-router.get('/',contribution.getAll)
+router.get('/',auth(endpoint.getAll),contribution.getAll)
 router.patch('/:id',auth(endpoint.update),myMulter(fileValidation.imag).single('image'),contribution.updateContribution)
 router.post('/',auth(endpoint.createContribution), myMulter(fileValidation.imag).single('image'),validation(validateContribution.createContribution),contribution.createContribution)
 export default router
